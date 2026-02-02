5f9800537ed8f3f53203dc76b799520d5ebde5c1
Dmytro Mitin <dmitin3@gmail.com>
2026-02-02 22:19:31 +0200

    Refactor stdin handling with Option.when

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,13 @@
 object Main extends App {
   val defaultMessage = "Hello, world!"
+  val stdinMessage =
+    Option.when(System.in.available() > 0) {
+      scala.io.StdIn.readLine()
+    }.filter(_.nonEmpty)
   val message = args.headOption
     .orElse(sys.props.get("hello.message"))
     .orElse(sys.env.get("HELLO_MESSAGE"))
+    .orElse(stdinMessage)
     .getOrElse(defaultMessage)
   println(message)
 }
